fef5a0ce6d9a776ad6b59388794a3aff1224968b
souleymane <souleymanedialloo@yahoo.fr>
2022-01-14 18:08:10 +0100

    add collectstatic

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,4 +10,24 @@ urlpatterns = [
     path("profile/<uuid:profile_id>/", views.user_profile, name="user-profile"),
     path("dashboard/", views.dashboard, name="dashboard"),
     path('update/', views.profile_update, name="profile-update"),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name="accounts/password_reset.html"
+         ),
+         name="password_reset"),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name="accounts/password_reset_done.html"
+         ),
+         name="password_reset_done"),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name="accounts/password_reset_confirm.html"
+         ),
+         name="password_reset_confirm"),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name="accounts/password_reset_complete.html"
+         ),
+         name="password_reset_complete"),
 ]
