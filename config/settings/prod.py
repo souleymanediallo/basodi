568b60aea690e3814402dac87d979030ae8e04bf
souleymanediallo <souleymanedialloo@yahoo.fr>
2024-02-25 15:41:19 +0100

    up

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -10,22 +10,24 @@ AWS_DEFAULT_ACL = env("AWS_DEFAULT_ACL", default=None)
 AWS_S3_CUSTOM_DOMAIN = env("DJANGO_AWS_S3_CUSTOM_DOMAIN", default=None)
 aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
-
-STATIC_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/static/"
-MEDIA_URL = f"https://{aws_s3_domain}/media/"
-
-
+# New
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+#
+# STATIC_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/static/"
+# MEDIA_URL = f"https://{aws_s3_domain}/media/"
+#
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_S3_VERIFY = True
 
 # STATIC
 # ------------------------
-#STATICFILES_STORAGE = "utils.storages.StaticRootS3Boto3Storage"
-#STATIC_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/static/"
+STATICFILES_STORAGE = "utils.storages.StaticRootS3Boto3Storage"
+STATIC_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/static/"
 
 # MEDIA
 # ------------------------------------------------------------------------------
-#DEFAULT_FILE_STORAGE = "utils.storages.MediaRootS3Boto3Storage"
-#MEDIA_URL = f"https://{aws_s3_domain}/media/"
+DEFAULT_FILE_STORAGE = "utils.storages.MediaRootS3Boto3Storage"
+MEDIA_URL = f"https://{aws_s3_domain}/media/"
 
 
