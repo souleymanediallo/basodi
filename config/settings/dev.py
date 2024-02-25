d1683b4d35ea48f266254206f2c0451446f09c5f
souleymanediallo <souleymanedialloo@yahoo.fr>
2024-02-25 13:30:01 +0100

    change region

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -126,6 +126,9 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "mediafiles"
 
+# AWS S3 CONFIGURATIONS
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
